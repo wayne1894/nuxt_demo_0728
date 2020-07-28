7fc7fdd88722a02d789341358cfc8f73ca61a0fe
wayne1894 <mose286778@gmail.com>
2020-07-28 21:19:34 +0800

    no message

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header id="header">
+      <div class="inner">
+
+          <!-- Logo -->
+          
+              <nuxt-link to="/" class="logo">
+                  <span class="symbol"><img src="~/assets/images/logo.svg" alt="" /></span><span class="title">Phantom2222</span>
+              </nuxt-link>
+        
+
+          <!-- Nav -->
+              <nav>
+                  <ul>
+                      <li><a href="#menu">Menu</a></li>
+                  </ul>
+              </nav>
+
+      </div>
+  </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+			<div id="wrapper">
+
+				<!-- Header -->
+                <Header></Header>
+
+				<!-- Menu -->
+					<Menu></Menu>
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<header>
+								<h1>This is Phantom, a free, fully responsive site<br />
+								template designed by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+								<p>Etiam quis viverra lorem, in semper lorem. Sed nisl arcu euismod sit amet nisi euismod sed cursus arcu elementum ipsum arcu vivamus quis venenatis orci lorem ipsum et magna feugiat veroeros aliquam. Lorem ipsum dolor sit amet nullam dolore.</p>
+							</header>
+							<section class="tiles">
+								<article class="style1">
+									<span class="image">
+										<img src="~/assets/images/pic01.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Magna</h2>
+										<div class="content">
+											<p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+										</div>
+									</a>
+								</article>
+								
+							</section>
+						</div>
+					</div>
+
+				<!-- Footer -->
+					<Footer></Footer>
+			</div>
+</template>
+
+<script>
+import Header from "~/components/Header.vue";
+import Menu from "~/components/Menu.vue";
+import Footer from "~/components/Footer.vue"; 
+
+export default {
+  components: {
+    Header,
+    Menu,
+    Footer
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+  
+  
+</script>
