6529efcebc51d7638b536e01ec33ad18fc77b937
wayne1894 <mose286778@gmail.com>
2020-07-28 21:26:07 +0800

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,9 @@ export default {
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
   */
+  router: {
+    base: '/nuxt_demo_0728/'
+  },
   head: {
     title: process.env.npm_package_name || '',
     meta: [
@@ -25,11 +28,11 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     script:[
-      {src : "/js/jquery.min.js" , body: true},
-      {src : "/js/browser.min.js", body: true},
-      {src : "/js/breakpoints.min.js", body: true},
-      {src : "/js/util.js", body: true},
-      {src : "/js/main.js", body: true}
+      {src : "/nuxt_demo_0728/js/jquery.min.js" , body: true},
+      {src : "/nuxt_demo_0728/js/browser.min.js", body: true},
+      {src : "/nuxt_demo_0728/js/breakpoints.min.js", body: true},
+      {src : "/nuxt_demo_0728/js/util.js", body: true},
+      {src : "/nuxt_demo_0728/js/main.js", body: true}
     ]
   },
   /*
